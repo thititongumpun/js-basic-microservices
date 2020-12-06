46f410182138f22e5d5aa23463f51819af9f1d19
Thiti <thiti.adev@gmail.com>
2020-12-06 12:57:09 +0700

    k8s comminicate

--- posts/index.js
+++ posts/index.js
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res) => {
     title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://eventbus-service:4005/events', {
     type: 'PostCreated',
     data: {
       id,
